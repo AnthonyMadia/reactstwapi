24cd37317e89fdefb8a2fe9559e0282593aa4360
Anthony Madia <aamadia24@gmail.com>
2022-03-17 12:59:57 -0400

    Stub up starship list

---
+++ src/pages/StarshipList/StarshipList.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const StarshipList = (props) => {
+
+    const [starships, setStarships] = useState([])
+
+    return ( 
+        <>
+            <h3>StarShip List!!!!</h3>
+        </>
+     );
+}
+ 
+export default StarshipList;
