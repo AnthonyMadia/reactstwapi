d298f2a48c5a2b1c18809a385ae20f419f2bf28f
Anthony Madia <aamadia24@gmail.com>
2022-03-17 13:21:58 -0400

    Pull starhips data, render to page, and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react';
 import NavBar from './components/NavBar/NavBar';
+import StarshipList from './pages/StarshipList/StarshipList';
 
 function App() {
+
+  const [starItems, setStarItems] = useState([
+    //leave path empty
+    {url: "", name: "Starship List"}
+  ])
 
   return (
     <>
       <NavBar />
+      <Routes>
+        <Route path="" element={<StarshipList />} />
+      </Routes>
     </>
   );
 }
--- src/pages/StarshipList/StarshipList.jsx
+++ src/pages/StarshipList/StarshipList.jsx
@@ -1,12 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom'
+import { getStarshipList } from "../../services/api-calls";
+
 
 const StarshipList = (props) => {
 
-    const [starships, setStarships] = useState([])
+    const [starships, setStarships] = useState(['CR90 corvette'])
+
+    useEffect(() => {
+        getStarshipList()
+        .then(shipData => setStarships(shipData.results))
+    }, [])
 
     return ( 
         <>
             <h3>StarShip List!!!!</h3>
+            <div className="icon-container">
+                {starships.map((ship, idx) => (
+                    <div id="classDiv" className="card " key={idx}>
+                        {ship.name}
+                    </div>
+                ))}
+            </div>
         </>
      );
 }
