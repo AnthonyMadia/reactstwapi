e2b4298383c960a854c2b20007503f96c038f92f
Anthony Madia <aamadia24@gmail.com>
2022-03-17 11:51:06 -0400

    Add navbar and stlying

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+const NavBar = (props) => {
+    return (
+        <header className="App-header">
+          <h1>STAR WARS STARSHIPS</h1>
+        </header>
+    );
+  };
+  
+  export default NavBar;
